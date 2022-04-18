6b103f2545e1426082da8adcae1cff2e8ead2261
ADUNSW\z3530200 <d.ara@blackdog.org.au>
2022-04-19 02:23:55 +1000

    responsiveness done

--- src/Pages/Home/Home/Speciality/Specialty.js
+++ src/Pages/Home/Home/Speciality/Specialty.js
@@ -5,16 +5,16 @@ const Specialty = () => {
     return (
         <div id="speciality" className='container'>
             <h1 style={{marginTop:'30px'}}>My Speciality</h1>
-            <div className='services-container w-100' style={{marginBottom:'130px'}}>
+            <div className='services-container special w-100' style={{marginBottom:'130px'}}>
                 <div className='d-flex  justify-content-evenly mb-150px'>
-                    <ul>
+                    <ul className='ul1'>
                         <li>Easy booking process</li>
                         <li>Competitive package prices</li>
                         <li>Very minimum initial booking</li>
                     </ul>
                 </div>
                 <div>
-                    <ul>
+                    <ul className='ul2'>
                         <li>Edited soft copies of all photos</li>
                         <li>Free cloud backup for your precious memories</li>
                     </ul>
