bb6cd50e664e76b162b0a1b78c26bd3f5d835459
ADUNSW\z3530200 <d.ara@blackdog.org.au>
2022-04-18 13:46:19 +1000

    login and register form design

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import {Form, Button} from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import Register from '../Register/Register';
+import './Login.css';
+
 
 const Login = () => {
+
+    const navigate = useNavigate();
+    const navigateRegister = () =>{
+        navigate('/register');
+    }
+
     return (
-        <div>
-            <h2>this is login</h2>
-        </div>
+        <div className='container w-50 mx-auto border rounded mt-3'>
+         <h2 className='text-center pt-2 text-color'>Please Login</h2>   
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+            
+            <Form.Control type="email" name="email" placeholder="Enter email" required/>
+           
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+           
+            <Form.Control type="password" name="password" placeholder="Password" required/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            {/* <Form.Check type="checkbox" label="Check me out" /> */}
+            </Form.Group>
+            <Button className='bg-color' type="submit">
+            Login
+            </Button>
+            
+        </Form>
+        <p className='mt-2'>New to Click By Me? <Link to="/register" className='text-decoration-none fw-bold text-color' onClick={navigateRegister}>Please Register</Link></p>
+    </div>
     );
 };
 
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,10 +1,44 @@
 import React from 'react';
-
+import {Form, Button} from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 const Register = () => {
+
+
+  const navigate = useNavigate();
+    const navigateRegister = () =>{
+        navigate('/login');
+    }
+
+
     return (
-       <div>register</div>
+      <div className='container w-50 mx-auto border rounded mt-3'>
+      <h2 className='text-center pt-2 text-color'>Please Register</h2>   
+     <Form>
+         <Form.Group className="mb-3" controlId="formBasicEmail">
+         
+         <Form.Control type="email" name="email" placeholder="Enter email" required/>
+        
+         </Form.Group>
+
+         <Form.Group className="mb-3" controlId="formBasicPassword">
+        
+         <Form.Control type="password" name="password" placeholder="Password" required/>
+         </Form.Group>
+         <Form.Group className="mb-3" controlId="formBasicPassword">
+        
+         <Form.Control type="password" name="confirm-password"  placeholder="Confirm Password" required/>
+         </Form.Group>
+         <Form.Group className="mb-3" controlId="formBasicCheckbox">
+         {/* <Form.Check type="checkbox" label="Check me out" /> */}
+         </Form.Group>
+         <Button className='bg-color' type="submit">
+         Register
+         </Button>
+     </Form>
+     <p className='mt-2'>Already have an account? <Link to="/login" className='text-decoration-none fw-bold text-color' onClick={navigateRegister}>Please Login</Link></p>
+ </div>
     );
 };
 
