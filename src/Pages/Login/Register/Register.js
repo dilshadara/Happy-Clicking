cec6ea6e9ca806868387516e42b31e772d7acc50
ADUNSW\z3530200 <d.ara@blackdog.org.au>
2022-04-18 14:26:41 +1000

    User creation with email and password

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,42 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Form, Button} from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 
 const Register = () => {
 
+  const [error, setError]=useState('');
+  const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth);
 
   const navigate = useNavigate();
     const navigateRegister = () =>{
         navigate('/login');
     }
 
+    const handleRegister = (event) => {
+      event.preventDefault();
+      const email=event.target.email.value;
+      const password = event.target.password.value;
+      const confirmPassword = event.target.confirmPassword.value;
+
+      if(password.length<6){
+        setError('Password should be at least 6 characters long.');
+        return;
+      }
+
+      if(password !== confirmPassword){
+        setError('Password does not match.');
+        console.log(password,confirmPassword);
+        return;
+      }
+
+      createUserWithEmailAndPassword(email,password);
+
+
+    }
+
 
     return (
       <div className='container w-50 mx-auto border rounded mt-3'>
       <h2 className='text-center pt-2 text-color'>Please Register</h2>   
-     <Form>
-         <Form.Group className="mb-3" controlId="formBasicEmail">
-         
-         <Form.Control type="email" name="email" placeholder="Enter email" required/>
-        
-         </Form.Group>
-
-         <Form.Group className="mb-3" controlId="formBasicPassword">
-        
-         <Form.Control type="password" name="password" placeholder="Password" required/>
-         </Form.Group>
-         <Form.Group className="mb-3" controlId="formBasicPassword">
-        
-         <Form.Control type="password" name="confirm-password"  placeholder="Confirm Password" required/>
-         </Form.Group>
-         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-         {/* <Form.Check type="checkbox" label="Check me out" /> */}
-         </Form.Group>
-         <Button className='bg-color' type="submit">
-         Register
-         </Button>
-     </Form>
+        <Form onSubmit={handleRegister}>
+          
+            <Form.Control type="email" name="email" placeholder="Enter email" required className='mb-3'/>
+            
+            <Form.Control type="password" name="password" placeholder="Password" className='mb-3'/>
+            
+            
+            <Form.Control type="password" name="confirmPassword"  placeholder="Confirm Password" className='mb-3'/>
+            
+            <Button className='bg-color' type="submit">
+            Register
+            </Button>
+        </Form>    
+     <p className='text-danger mt-1'>{error}</p>
      <p className='mt-2'>Already have an account? <Link to="/login" className='text-decoration-none fw-bold text-color' onClick={navigateRegister}>Please Login</Link></p>
  </div>
     );
